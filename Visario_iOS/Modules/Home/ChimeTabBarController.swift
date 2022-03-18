23b16ef5123273813a9fc367f16b4334a50b87c9
Vitaliy Butsan <skolkojvastut@gmail.com>
2022-03-18 13:40:45 +0200

    Removed tab bar translucent

--- Visario_iOS/Modules/Home/ChimeTabBarController.swift
+++ Visario_iOS/Modules/Home/ChimeTabBarController.swift
@@ -14,6 +14,7 @@ final class ChimeTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tabBar.isTranslucent = false
         addTabs()
     }
     
